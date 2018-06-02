7c9c19b9b62b159b18158e1017a68547a8078983
Kiley Naro <kiley.naro@gmail.com>
2018-06-02 17:41:53 -0400

    P5JS sketch exists

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello world2!');
+import p5 from 'p5'
+
+const sketch = p5 => {
+    p5.setup = () => {
+        p5.createCanvas(800, 600)
+        p5.background(51)
+    }
+}
+
+new p5(sketch)
